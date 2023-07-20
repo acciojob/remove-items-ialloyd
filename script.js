dad4471ed135ba04e36977e350695b813d156ae2
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-20 18:15:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let colorSelect = document.getElementById('colorSelect');
+    let button = document.querySelector('input[type="button"]');
+
+    button.addEventListener('click', function() {
+        colorSelect.remove(colorSelect.selectedIndex);
+    });
+
